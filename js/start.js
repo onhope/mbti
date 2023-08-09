1c960530c7a1a15f0ea1561d5dba0094b41e63e4
onhope <chch4934@gmail.com>
2023-08-09 18:24:45 +0900

    qna 구현하기

--- js/start.js
+++ js/start.js
@@ -1,6 +1,32 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
+function addAnswer(answerText, qIdx) {
+  var a = document.querySelector(".answerBox");
+  var answer = document.createElement("button");
+  answer.classList.add("answerList")
+  a.appendChild(answer);
+  answer.innerHTML = answerText;
+
+  answer.addEventListener("click", function() {
+    var children = document.querySelectorAll(".answerList");
+    for (let i=0; i < children.length; i++) {
+      children[i].disabled = true;
+      children[i].style.display = "none";
+    }
+    goNext(++qIdx);
+  } , false);
+}
+
+function goNext(qIdx) {
+    var q = document.querySelector(".qBox");
+    q.innerHTML = qnaList[qIdx].q;
+
+    for (let i in qnaList[qIdx].a) {
+      addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+    }
+}
+
 function begin() {
 
   main.style.animation = "fadeOut 1s"
@@ -13,5 +39,7 @@ function begin() {
       main.style.display = "none"
       qna.style.display = "block"
     }, 450);
+    var qIdx = 0;
+    goNext(qIdx)
   }, 450);
 }
