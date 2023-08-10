97387cf60fdd5db2d884ace293016e8998d8f1e7
onhope <chch4934@gmail.com>
2023-08-11 01:13:00 +0900

    Result 구현하기(1)

--- js/start.js
+++ js/start.js
@@ -1,10 +1,25 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
+const result = document.querySelector("#result");
 const endpoint = 12;
 
+function goResult() {
+  qna.style.animation = "fadeOut 1s"
+  qna.style.WebkitAnimation = "fadeOut 1s"
+
+  setTimeout(function () {
+    result.style.animation = "fadeIn 1s"
+    result.style.WebkitAnimation = "fadeIn 1s"      
+    setTimeout(function () {
+      qna.style.display = "none"
+      result.style.display = "block"
+    }, 450);
+  }, 450);
+}
+
 function addAnswer(answerText, qIdx) {
-  var a = document.querySelector(".answerBox");
-  var answer = document.createElement("button");
+  let a = document.querySelector(".answerBox");
+  let answer = document.createElement("button");
   answer.classList.add("answerList")
   answer.classList.add("my-3");
   answer.classList.add("py-3");
@@ -15,7 +30,7 @@ function addAnswer(answerText, qIdx) {
   answer.innerHTML = answerText;
 
   answer.addEventListener("click", function() {
-    var children = document.querySelectorAll(".answerList");
+    let children = document.querySelectorAll(".answerList");
     for (let i=0; i < children.length; i++) {
       children[i].disabled = true;
       children[i].style.animation = "fadeOut 0.5s"
@@ -31,13 +46,18 @@ function addAnswer(answerText, qIdx) {
 }
 
 function goNext(qIdx) {
-    var q = document.querySelector(".qBox");
+    if (qIdx+1 === endpoint) {
+      goResult();
+      return;
+    }
+    let q = document.querySelector(".qBox");
     q.innerHTML = qnaList[qIdx].q;
 
     for (let i in qnaList[qIdx].a) {
       addAnswer(qnaList[qIdx].a[i].answer, qIdx);
     }
-    var status = document.querySelector(".statusBar");
+
+    let status = document.querySelector(".statusBar");
     status.style.width = (100/endpoint)*(qIdx+1)+'%';
 }
 
@@ -53,7 +73,7 @@ function begin() {
       main.style.display = "none"
       qna.style.display = "block"
     }, 450);
-    var qIdx = 0;
+    let qIdx = 0;
     goNext(qIdx)
   }, 450);
 }
